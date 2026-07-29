03ae9e6d071c9b5d988b7cea1c851dcf2fae4ddc
CossyMarine <shidayawatukibwari@gmail.com>
2026-07-29 23:31:26 +0300

    Update settingsRoutes.js

--- routes/settingsRoutes.js
+++ routes/settingsRoutes.js
@@ -1,12 +1,12 @@
 // routes/settingsRoutes.js
 import express from "express";
-import { protect, authorize, requirePermission } from "../Middlewares/authMiddleware.js";
+import { protect, authorize, checkPermission } from "../Middlewares/auth.js";
 import { getSettings, updateSettings, getPublicSettings } from "../controllers/settingsController.js";
 
 const router = express.Router();
 
 router.get("/public", getPublicSettings);
-router.get("/", protect, authorize("admin", "accountant"), requirePermission("settings"), getSettings);
-router.patch("/", protect, authorize("admin", "accountant"), requirePermission("settings"), updateSettings);
+router.get("/", protect, authorize("admin"), checkPermission("settings"), getSettings);
+router.patch("/", protect, authorize("admin"), checkPermission("settings"), updateSettings);
 
 export default router;
